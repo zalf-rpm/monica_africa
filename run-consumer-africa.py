fd6dd341999216e44364f564cc2dd9700c1a9c6b
Awa FAYE <evaf90@hotmail.fr>
2023-09-20 15:55:14 +0200

    Update run-consumer-africa.py
    Add ETa/ETc in output_grids

--- run-consumer-africa.py
+++ run-consumer-africa.py
@@ -89,6 +89,7 @@ def write_row_to_grids(row_col_data, row, col_0, no_of_cols, header, path_to_out
         "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         "AbBiom": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         "LAI": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
+        "ETa/ETc": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "tradefavg": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "heatredavg": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "frostredavg": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
