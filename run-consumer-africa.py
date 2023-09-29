67c37bf7a1a86842cabde0fec2b8398f17207544
Awa FAYE <evaf90@hotmail.fr>
2023-09-29 10:01:33 +0200

    Update run-consumer-africa.py

--- run-consumer-africa.py
+++ run-consumer-africa.py
@@ -88,7 +88,7 @@ def write_row_to_grids(row_col_data, row, col_0, no_of_cols, header, path_to_out
     output_grids = {
         "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         "ETa_ETc": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
-        "AbBiom": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
+       # "AbBiom": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
        #  "LAI": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "tradefavg": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
         # "heatredavg": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
